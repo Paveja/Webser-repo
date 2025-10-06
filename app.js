9f11ed798231d89ddd1c4b4ec0a2c47afcf1697f
sri.srisailam <sri.srisailam@rootquotient.com>
2025-10-06 16:39:41 +0530

    added new env v ariables

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post("/upload", async (req, res) => {
       return res.status(400).send("No file received");
     }
 
-    const bucketName = "revolte-ramco-code-red";
+    const bucketName = process.env.BUCKET_NAME;
     const key = "apple.png";
 
     // Upload to S3
